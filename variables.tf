3cd34dd155aa385fc07f213e8bb806750c7309fe
chai <chailand@chais-MacBook-Air.local>
2025-11-22 12:08:22 +1100

    Initial commit: Azure Terraform project structure

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+  default     = "example-resources"
+}
+
+variable "location" {
+  description = "The Azure region to deploy resources"
+  type        = string
+  default     = "East US"
+}
